db3d113b2c2abb4afcb5024e8c95a118bec790a0
Felipe Becalli T. <felipebecallitrindade@gmail.com>
2025-09-04 06:22:45 -0300

    Update PostDetail.vue
    Se não carregar por algum motivo, Carregando na tela...
    Remover o console.log após "aprender" e criar um except direito

--- frontend/src/views/PostDetail.vue
+++ frontend/src/views/PostDetail.vue
@@ -1,19 +1,15 @@
 <template>
   <div v-if="post" class="p-6 max-w-3xl mx-auto">
     <h1 class="text-3xl font-bold text-gray-900">{{ post.titulo }}</h1>
-
-    <!-- Exibir imagem, se existir -->
     <img
       v-if="post.thumbnail"
       :src="'http://127.0.0.1:8000' + post.thumbnail"
       :alt="post.titulo"
       class="w-full h-64 object-cover my-4 rounded"
     >
-
-    <!-- Conteúdo do post -->
     <div class="text-gray-800 whitespace-pre-line">
-        
-        {{ post.conteudo }}</div>
+        {{ post.conteudo }}
+    </div>
   </div>
   <div v-else class="p-6 text-center">
     Carregando...
